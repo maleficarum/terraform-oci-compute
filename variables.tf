3d6d999f83f14964898c9e326b1b0ad09b5f1ef2
Oscar Ivan <o@maleficarum.mx>
2025-04-30 10:09:01 -0600

    Add base structure

---
+++ variables.tf
@@ -0,0 +1,29 @@
+# tflint-ignore: terraform_unused_declarations
+variable "instance_configuration" {
+  description = "Instance parameters"
+  type = list(
+    object({
+      name                      = string,
+      ssh_public_key            = string,
+      assign_ipv6ip             = string,
+      assign_private_dns_record = string,
+      assign_public_ip          = string,
+      shape_config = object({
+        type   = string,
+        memory = number,
+        ocpus  = number
+      }),
+      image = string
+    })
+  )
+}
+
+variable "compartment_id" {
+  type        = string
+  description = "The compartment OCID to deploy the instances"
+}
+
+variable "subnet" {
+  type        = string
+  description = "The ID of the target subnet"
+}
